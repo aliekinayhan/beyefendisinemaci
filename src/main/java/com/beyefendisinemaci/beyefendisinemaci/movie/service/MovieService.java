2803785fe84bff3672a26eda4dfbb672dc547027
aliekinayhan <ayhanaliekin0@gmail.com>
2026-04-28 14:09:53 +0300

    feat: add MovieService

--- src/main/java/com/beyefendisinemaci/beyefendisinemaci/movie/service/MovieService.java
+++ src/main/java/com/beyefendisinemaci/beyefendisinemaci/movie/service/MovieService.java
@@ -1,4 +1,50 @@
 package com.beyefendisinemaci.beyefendisinemaci.movie.service;
 
+import com.beyefendisinemaci.beyefendisinemaci.movie.entity.Movie;
+import com.beyefendisinemaci.beyefendisinemaci.movie.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class MovieService {
+
+    private final MovieRepository repository;
+
+    public List<Movie> getAllMovies() {
+        return repository.findAll();
+    }
+
+    public List<Movie> getMovieByTitle(String title) {
+        return repository.findByTitleContainingIgnoreCase(title);
+    }
+
+    public Movie getMovieById(UUID id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    public Movie createMovie(Movie movie) {
+        return repository.save(movie);
+    }
+
+    public Movie updateMovie(UUID id, Movie updatedMovie) {
+        Movie existingMovie = repository.findById(id).orElseThrow();
+        existingMovie.setTitle(updatedMovie.getTitle());
+        existingMovie.setGenre(updatedMovie.getGenre());
+        existingMovie.setPosterUrl(updatedMovie.getPosterUrl());
+        existingMovie.setReview(updatedMovie.getReview());
+        existingMovie.setVideoUrl(updatedMovie.getVideoUrl());
+        existingMovie.setReleaseYear(updatedMovie.getReleaseYear());
+        existingMovie.setTmdbId(updatedMovie.getTmdbId());
+        return repository.save(existingMovie);
+    }
+
+    public void deleteMovieById(UUID id) {
+        repository.deleteById(id);
+    }
+
+
 }
