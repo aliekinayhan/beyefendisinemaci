4d47b9cbfd7d82a5bf06b0bd0798fefc8067706c
aliekinayhan <ayhanaliekin0@gmail.com>
2026-04-28 13:36:23 +0300

    feat: add MovieRepository with search method

--- src/main/java/com/beyefendisinemaci/beyefendisinemaci/movie/repository/MovieRepository.java
+++ src/main/java/com/beyefendisinemaci/beyefendisinemaci/movie/repository/MovieRepository.java
@@ -1,4 +1,19 @@
 package com.beyefendisinemaci.beyefendisinemaci.movie.repository;
 
-public interface MovieRepository    {
+import com.beyefendisinemaci.beyefendisinemaci.movie.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, UUID> {
+    /*
+    SQL
+    findBy -> where
+    Title -> search in here
+    Containing -> like %..%
+    */
+    List<Movie> findByTitleContainingIgnoreCase(String title);
 }
