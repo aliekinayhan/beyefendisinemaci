053ab8e3c5a682780aabbe210d9b723206b218ba
aliekinayhan <ayhanaliekin0@gmail.com>
2026-04-28 14:34:42 +0300

    feat: add MovieController

--- src/main/java/com/beyefendisinemaci/beyefendisinemaci/movie/controller/MovieController.java
+++ src/main/java/com/beyefendisinemaci/beyefendisinemaci/movie/controller/MovieController.java
@@ -1,4 +1,46 @@
 package com.beyefendisinemaci.beyefendisinemaci.movie.controller;
 
+import com.beyefendisinemaci.beyefendisinemaci.movie.entity.Movie;
+import com.beyefendisinemaci.beyefendisinemaci.movie.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/movies")
+@RequiredArgsConstructor
 public class MovieController {
+
+    private final MovieService service;
+
+    @GetMapping()
+    public List<Movie> getAllMovies() {
+        return service.getAllMovies();
+    }
+
+    @GetMapping("/search")
+    public List<Movie> getMovieByTitle(@RequestParam String title) {
+        return service.getMovieByTitle(title);
+    }
+
+    @GetMapping("/{id}")
+    public Movie getMovieById(@PathVariable UUID id) {
+        return service.getMovieById(id);
+    }
+
+    @PostMapping()
+    public Movie createMovie(@RequestBody Movie movie) {
+        return service.createMovie(movie);
+    }
+
+    @PutMapping("/{id}")
+    public Movie updateMovie(@PathVariable UUID id, @RequestBody Movie movie) {
+        return service.updateMovie(id,movie);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMovie (@PathVariable UUID id){
+        service.deleteMovieById(id);
+    }
 }
