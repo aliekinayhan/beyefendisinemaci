7d734ffda3b270119efd11ec9dd085eeb21c4429
aliekinayhan <ayhanaliekin0@gmail.com>
2026-04-28 12:18:04 +0300

    chore: update gitignore

---
+++ src/main/java/com/beyefendisinemaci/beyefendisinemaci/BeyefendisinemaciApplication.java
@@ -0,0 +1,13 @@
+package com.beyefendisinemaci.beyefendisinemaci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyefendisinemaciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyefendisinemaciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beyefendisinemaci/beyefendisinemaci/BeyefendisinemaciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyefendisinemaci.beyefendisinemaci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeyefendisinemaciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
