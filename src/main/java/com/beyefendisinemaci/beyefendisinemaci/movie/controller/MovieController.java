311561c185aab1ef895494c7625ac5df819ea9c3
aliekinayhan <ayhanaliekin0@gmail.com>
2026-04-30 11:18:15 +0300

    feat: update MovieController to use ResponseEntity

--- src/main/java/com/beyefendisinemaci/beyefendisinemaci/movie/controller/MovieController.java
+++ src/main/java/com/beyefendisinemaci/beyefendisinemaci/movie/controller/MovieController.java
@@ -4,6 +4,8 @@ import com.beyefendisinemaci.beyefendisinemaci.movie.dto.request.MovieRequestDto
 import com.beyefendisinemaci.beyefendisinemaci.movie.dto.response.MovieResponseDto;
 import com.beyefendisinemaci.beyefendisinemaci.movie.service.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
@@ -16,32 +18,33 @@ public class MovieController {
     private final MovieService service;
 
     @GetMapping()
-    public List<MovieResponseDto> getAllMovies() {
-        return service.getAllMovies();
+    public ResponseEntity<List<MovieResponseDto>> getAllMovies() {
+        return ResponseEntity.ok(service.getAllMovies());
     }
 
     @GetMapping("/search")
-    public List<MovieResponseDto> getMovieByTitle(@RequestParam String q) {
-        return service.getMovieByTitle(q);
+    public ResponseEntity<List<MovieResponseDto>> getMovieByTitle(@RequestParam String q) {
+        return ResponseEntity.ok(service.getMovieByTitle(q));
     }
 
     @GetMapping("/{id}")
-    public MovieResponseDto getMovieById(@PathVariable UUID id) {
-        return service.getMovieById(id);
+    public ResponseEntity<MovieResponseDto> getMovieById(@PathVariable UUID id) {
+        return ResponseEntity.ok(service.getMovieById(id));
     }
 
     @PostMapping()
-    public MovieResponseDto createMovie(@RequestBody MovieRequestDto movie) {
-        return service.createMovie(movie);
+    public ResponseEntity<MovieResponseDto> createMovie(@RequestBody MovieRequestDto movie) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.createMovie(movie));
     }
 
     @PutMapping("/{id}")
-    public MovieResponseDto updateMovie(@PathVariable UUID id, @RequestBody MovieRequestDto movie) {
-        return service.updateMovie(id,movie);
+    public ResponseEntity<MovieResponseDto> updateMovie(@PathVariable UUID id, @RequestBody MovieRequestDto movie) {
+        return ResponseEntity.ok(service.updateMovie(id,movie));
     }
 
     @DeleteMapping("/{id}")
-    public void deleteMovie (@PathVariable UUID id){
+    public ResponseEntity<Void> deleteMovie (@PathVariable UUID id){
         service.deleteMovieById(id);
+        return ResponseEntity.noContent().build();
     }
 }
