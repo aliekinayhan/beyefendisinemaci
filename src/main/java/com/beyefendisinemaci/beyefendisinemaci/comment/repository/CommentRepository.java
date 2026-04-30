6904d1b770997406fe91668add19cd13503516e1
aliekinayhan <ayhanaliekin0@gmail.com>
2026-04-30 13:41:52 +0300

    feat: add CommentRepository

--- src/main/java/com/beyefendisinemaci/beyefendisinemaci/comment/repository/CommentRepository.java
+++ src/main/java/com/beyefendisinemaci/beyefendisinemaci/comment/repository/CommentRepository.java
@@ -1,4 +1,18 @@
 package com.beyefendisinemaci.beyefendisinemaci.comment.repository;
 
-public class CommentRepository {
+import com.beyefendisinemaci.beyefendisinemaci.comment.entity.Comment;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, UUID> {
+
+    Slice<Comment> findByMovieId(UUID movieId, Pageable pageable);
+
+    List<Comment> findByUserId(UUID userId);
 }
