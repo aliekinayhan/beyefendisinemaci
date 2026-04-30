c4174d2131442a22ee5e13793d47b851dbdc6695
aliekinayhan <ayhanaliekin0@gmail.com>
2026-04-30 13:34:36 +0300

    feat: add CommentMapper

--- src/main/java/com/beyefendisinemaci/beyefendisinemaci/comment/mapper/CommentMapper.java
+++ src/main/java/com/beyefendisinemaci/beyefendisinemaci/comment/mapper/CommentMapper.java
@@ -1,4 +1,17 @@
 package com.beyefendisinemaci.beyefendisinemaci.comment.mapper;
 
-public class CommentMapper {
+import com.beyefendisinemaci.beyefendisinemaci.comment.dto.request.CommentRequestDto;
+import com.beyefendisinemaci.beyefendisinemaci.comment.dto.response.CommentResponseDto;
+import com.beyefendisinemaci.beyefendisinemaci.comment.entity.Comment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+    @Mapping(source = "movie.id",target = "movieId")
+    @Mapping(source = "user.id",target = "userId")
+    @Mapping(source = "user.username",target = "username")
+    @Mapping(source = "user.profilePhoto",target = "userProfilePhoto")
+    CommentResponseDto toResponseDto (Comment comment);
+    Comment toEntity (CommentRequestDto requestDto);
 }
