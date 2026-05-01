077ffee9393cee2dab69115b7b439b30a4a7efe2
aliekinayhan <ayhanaliekin0@gmail.com>
2026-05-01 12:12:58 +0300

    feat: add RefreshTokenRepository

---
+++ src/main/java/com/beyefendisinemaci/beyefendisinemaci/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,17 @@
+package com.beyefendisinemaci.beyefendisinemaci.auth.repository;
+
+import com.beyefendisinemaci.beyefendisinemaci.auth.entity.RefreshToken;
+import com.beyefendisinemaci.beyefendisinemaci.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+    // Find by token instead of user, because if we searched by userId,
+    // an attacker could send any userId to access other accounts.
+    Optional<RefreshToken> findByToken(String token);
+    void deleteByUser(User user);
+}
